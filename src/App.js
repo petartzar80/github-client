8322b167926306827ba6c2723bb20b60189d5431
Petar Tzar <petartzar80@gmail.com>
2020-03-12 19:12:28 +0100

    basic setup, getting data from github

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { useQuery, ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient, { gql } from 'apollo-boost';
+import 'dotenv/config';
 
-function App() {
+const GET_USER_REPO = gql`
+  {
+    user(login: "egoist") {
+      name
+      repository(name: "poi") {
+        name
+      }
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${
+          process.env.REACT_APP_GITHUB_TOKEN
+          }`,
+      },
+    });
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      GitHub Client
-    </div>
+    <ApolloProvider client={client}>
+      <Repo />
+    </ApolloProvider>
+  );
+}
+
+const Repo = () => {
+  const { loading, error, data } = useQuery(GET_USER_REPO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.toString}</p>;
+
+  return (
+    <p>You are exploring repo "{data.user.repository.name}" by {data.user.name}.</p>
   );
 }
 
