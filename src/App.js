1617ea533c90d5528a54867641c72d2e16b6e399
Petar Tzar <petartzar80@gmail.com>
2020-03-13 07:37:53 +0100

    introduced form inputs, Repo component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,121 @@
 import React from 'react';
+import { useState } from 'react';
+
 import { useQuery, ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient, { gql } from 'apollo-boost';
 import 'dotenv/config';
 
+import './styles.css';
+
 const GET_USER_REPO = gql`
-  {
-    user(login: "egoist") {
+  query ($user: String!, $repo: String!) {
+    user(login: $user) {
       name
-      repository(name: "poi") {
+      repository(name: $repo) {
         name
       }
     }
   }
 `;
 
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${
-          process.env.REACT_APP_GITHUB_TOKEN
-          }`,
-      },
-    });
-  },
-});
-
 const App = () => {
+  const [token, setToken] = useState('');
+  const [path, setPath] = useState('');
+  const [client, setClient] = useState();
+  const [user, setUser] = useState();
+  const [repo, setRepo] = useState();
+
+  const handleChangeToken = e => {
+    setToken(e.target.value);
+  }
+
+  const handleChangePath = e => {
+    setPath(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setClient(new ApolloClient({
+      uri: 'https://api.github.com/graphql',
+      request: operation => {
+        operation.setContext({
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        });
+      },
+    }));
+
+    const [user, repo] = path.split('/');
+    setUser(user);
+    setRepo(repo);
+  }
+
   return (
-    <ApolloProvider client={client}>
-      <Repo />
-    </ApolloProvider>
+    <div className="container">
+      <header className="header">
+        <h1>GitHub Client</h1>
+      </header>
+
+      <div className="form-container">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="form-content">
+            <div className="form-input">
+              <label htmlFor="githubToken">
+                Please, enter your personal OAuth GitHub token:
+              </label>
+              <input
+                id="githubToken"
+                type="text"
+                value={token}
+                onChange={(e) => handleChangeToken(e)}
+                style={{ width: '300px' }}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label htmlFor="url">
+                Get issues from https://github.com/
+              </label>
+              <input
+                id="url"
+                type="text"
+                value={path}
+                onChange={(e) => handleChangePath(e)}
+                style={{ width: '300px' }}
+                required
+              />
+            </div>
+            <div className="form-button">
+              <button type="submit">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      {client &&
+        <ApolloProvider client={client}>
+          <Repo vars={[user, repo]} />
+        </ApolloProvider>
+      }
+    </div>
   );
 }
 
-const Repo = () => {
-  const { loading, error, data } = useQuery(GET_USER_REPO);
+const Repo = ({ vars }) => {
+  const [user, repo] = vars;
+  const { loading, error, data } = useQuery(GET_USER_REPO, {
+    variables: { user, repo },
+  });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.toString}</p>;
+  if (error) return <p>Error.</p>;
 
   return (
-    <p>You are exploring repo "{data.user.repository.name}" by {data.user.name}.</p>
+    <div className="repo-container">
+      <p>You are exploring repo "{data.user.repository.name}" by {data.user.name}.</p>
+    </div>
   );
 }
 
